48ef448fc8baeaf142bba33e5fd65ceb9e6c4619
policomputo <admonservicioalcliente@gmail.com>
2026-04-30 17:55:19 -0500

    Creando estructura de paginas

--- antigravity/playground/retrograde-cassini/pages/index.js
+++ antigravity/playground/retrograde-cassini/pages/index.js
@@ -1,8 +1,10 @@
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <h1>¡App funcionando!</h1>
       <p>Conectada a Aiven y desplegada en Netlify.</p>
     </div>
-  )
-}
+  );
+};
+
+module.exports = Home; // Cambiamos "export default" por "module.exports"
